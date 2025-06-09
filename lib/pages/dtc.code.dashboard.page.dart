0ac32823ffd0b64554f0d60f1b346ba52a5f74ec
atajr.650 <atajr.650@gmail.com>
2025-06-09 20:24:25 +0000

    Implementado menu Drawer na dashboard

--- lib/pages/dtc.code.dashboard.page.dart
+++ lib/pages/dtc.code.dashboard.page.dart
@@ -1,3 +1,4 @@
+import 'package:dtc_harleys_app/common/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:dtc_harleys_app/pages/dtc.code.access.oldmodels.page.dart';
 import 'package:dtc_harleys_app/pages/dtc.code.know.more.page.dart';
@@ -5,16 +6,15 @@ import 'package:dtc_harleys_app/pages/dtc.code.list.page.dart';
 import 'package:dtc_harleys_app/pages/dtc.code.abbreviation.list.page.dart';
 import 'package:dtc_harleys_app/pages/dtc.code.access.newmodels.page.dart';
 
-const String _labelAppTitle = 'Códigos DTC Harleys';
-
+const String _labelAppTitle = 'DTC Harleys App';
+const String _pathLogo = 'lib/assets/imgs/codigosdtc.png';
+const String _labelPrivacy = 'Política de privacidade';
 const List<String> _listAdmScreens = ["Códigos DTC", "Siglas", "Saiba mais"];
-
 const List<String> _listAdmScreensDesc = [
   "Códigos de erro catalogados pela HD",
   "Siglas utilizadas nos erros",
   "Saiba mais sobre os Códigos DTC",
 ];
-
 const List<IconData> _listAdmIcons = [
   Icons.error_outline_rounded,
   Icons.abc_outlined,
@@ -38,8 +38,10 @@ class _DtcCodeDashboardPageState extends State<DtcCodeDashboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Stack(children: <Widget>[_dashBg(), _content(context),
-      ]),
+      drawer: _drawr(),
+      drawerEdgeDragWidth: 50,
+      drawerScrimColor: Colors.black87,
+      body: Stack(children: <Widget>[_dashBg(), _content(context)]),
     );
   }
 
@@ -65,10 +67,89 @@ class _DtcCodeDashboardPageState extends State<DtcCodeDashboardPage> {
     backgroundColor: Colors.deepOrange[300],
   );
 
+  _drawr() => Drawer(
+    child: ListView(
+      children: <Widget>[
+        Text(_labelAppTitle, textAlign: TextAlign.center),
+        UserAccountsDrawerHeader(
+          accountName: Text('Olá, Harleyro(a)'),
+          accountEmail: Text('gapsolutions.slz@gmail.com'),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: ExactAssetImage(_pathLogo),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.info),
+          title: Text('Sobre o DTC Harleys App'),
+          onTap: () {
+            showAboutDialog(
+              context: context,
+              applicationVersion: APP_VERSION,
+              applicationName: _labelAppTitle,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: const Text(
+                    'Este aplicativo é propriedade da Gap Solutions Slz.',
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: const Text(
+                    'Desenvolvedor: Adalberto Jr.',
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: const Text(
+                    'Email: gapsolutions.slz@gmail.com',
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2, bottom: 20),
+                  child: const Text(
+                    'Copyright 2025 Brasil',
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 100.0,
+                  child: Container(
+                    child: Image.asset(_pathLogo),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.privacy_tip),
+          title: Text(_labelPrivacy),
+          /*onTap: () async {
+            if (remotePDFpath.isNotEmpty) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PDFScreen(path: remotePDFpath),
+                ),
+              );
+            }
+          },*/
+        ),
+      ],
+    ),
+  );
+
   _dashBg() => Column(
     children: <Widget>[
       Expanded(child: Container(color: Colors.deepOrange[300])),
-      Expanded(flex: 2, child: Container(color: Colors.transparent),),
+      Expanded(flex: 2, child: Container(color: Colors.transparent)),
     ],
   );
 
